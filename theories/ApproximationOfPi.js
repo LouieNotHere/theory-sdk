ed2e9ad53f0ad585e654ab369e66c709a7f35110
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-03-07 17:51:48 +0800

    Update ApproximationOfPi.js

--- theories/ApproximationOfPi.js
+++ theories/ApproximationOfPi.js
@@ -4,14 +4,14 @@ import { BigNumber } from "./api/BigNumber";
 import { theory } from "./api/Theory";
 import { Utils } from "./api/Utils";
 
-var id = "pi_approximation_theory";
-var name = "Pi Approximation";
-var description = "The more digits of π you approximate correctly, the more ρ you generate!";
-var authors = "Custom by User";
-var version = 1;
+var id = "approximation_of_pi";
+var name = "Approximation of Pi";
+var description = "This theory focuses on approximating the value of pi. Will you be able to approximate more than the usual digits of pi?";
+var authors = "Louie Soulen Kurenai (@paytouse on Discord)";
+var version = 2;
 
 var currency;
-var piAccuracy;
+var piAccuracy, c1, c2;
 var piExp;
 
 var achievement1, achievement2;
@@ -20,44 +20,35 @@ var chapter1, chapter2;
 var init = () => {
     currency = theory.createCurrency();
 
-    ///////////////////
-    // Regular Upgrades
+    piAccuracy = theory.createUpgrade(0, currency, new ExponentialCost(10, Math.log2(3)));
+    piAccuracy.getDescription = (_) => Utils.getMath("π Digits: " + (piAccuracy.level + 1));
+    piAccuracy.getInfo = (amount) => Utils.getMathTo("Correct Digits: " + (piAccuracy.level + 1), "Correct Digits: " + (piAccuracy.level + amount + 1));
 
-    // piAccuracy - Improves π approximation
-    {
-        let getDesc = (level) => "π Digits: " + (level + 1);
-        let getInfo = (level) => "Correct Digits: " + (level + 1);
-        piAccuracy = theory.createUpgrade(0, currency, new ExponentialCost(10, Math.log2(3)));
-        piAccuracy.getDescription = (_) => Utils.getMath(getDesc(piAccuracy.level));
-        piAccuracy.getInfo = (amount) => Utils.getMathTo(getInfo(piAccuracy.level), getInfo(piAccuracy.level + amount));
-    }
+    c1 = theory.createUpgrade(1, currency, new ExponentialCost(15, Math.log2(2)));
+    c1.getDescription = (_) => Utils.getMath("c_1=" + getC1(c1.level).toString(2));
+    c1.getInfo = (amount) => Utils.getMathTo(getC1(c1.level).toString(2), getC1(c1.level + amount).toString(2));
 
-    /////////////////////
-    // Permanent Upgrades
+    c2 = theory.createUpgrade(2, currency, new ExponentialCost(20, Math.log2(2.5)));
+    c2.getDescription = (_) => Utils.getMath("c_2=" + getC2(c2.level).toString(2));
+    c2.getInfo = (amount) => Utils.getMathTo(getC2(c2.level).toString(2), getC2(c2.level + amount).toString(2));
+
     theory.createPublicationUpgrade(0, currency, 1e10);
     theory.createBuyAllUpgrade(1, currency, 1e13);
     theory.createAutoBuyerUpgrade(2, currency, 1e30);
 
-    ///////////////////////
-    //// Milestone Upgrades
     theory.setMilestoneCost(new LinearCost(10, 10));
 
-    {
-        piExp = theory.createMilestoneUpgrade(0, 3);
-        piExp.description = Localization.getUpgradeIncCustomExpDesc("π Bonus", "0.05");
-        piExp.info = Localization.getUpgradeIncCustomExpInfo("π Bonus", "0.05");
-        piExp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
-    }
-    
-    /////////////////
-    //// Achievements
-    achievement1 = theory.createAchievement(0, "First Digits", "Correctly approximate at least 3 digits of π.", () => piAccuracy.level >= 3);
-    achievement2 = theory.createSecretAchievement(1, "Mathematician", "Reach 10 correct digits of π!", "You're really into numbers!", () => piAccuracy.level >= 10);
+    piExp = theory.createMilestoneUpgrade(0, 3);
+    piExp.description = Localization.getUpgradeIncCustomExpDesc("π Bonus", "0.05");
+    piExp.info = Localization.getUpgradeIncCustomExpInfo("π Bonus", "0.05");
+    piExp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
 
-    ///////////////////
-    //// Story chapters
-    chapter1 = theory.createStoryChapter(0, "Pi Begins", "You start approximating π. A simple 3.1 isn't enough!", () => piAccuracy.level > 0);
-    chapter2 = theory.createStoryChapter(1, "A True Mathematician", "With enough digits, you become an expert in π.", () => piAccuracy.level > 5);
+    achievement1 = theory.createAchievement(0, "Basic Approximation", "Correctly approximate at least 3 digits of pi.", () => piAccuracy.level >= 3);
+    achievement2 = theory.createSecretAchievement(1, "Approximator", "Approximate the first 10 digits of pi.", "You're actually into approximating numbers, are you?", () => piAccuracy.level >= 10);
+
+    chapter1 = theory.createStoryChapter(0, "Prologue", "You are a young mathematician.\nYour professor tells you approximate the value of pi.\nAs you got home, you took a small research about it.\nFortunately, no one has approximated the actual value of pi.\nFor now, you are attempting to approximate the value of pi.\nWill you be able to reach the usual value?", () => piAccuracy.level >= 0);
+    chapter2 = theory.createStoryChapter(1, "The Approximation Begins", "You have reached the usual value.\nBut it seemed like it's not enough.\nYou asked your professor about this.\nHe advised you to try and approximate it further.\nSo you went to your desk, and try to do what he said.", () => piAccuracy.level >= 3);
+    chapter3 = theory.createStoryChapter(2, "Bizzare Numbers", "It took you a long time to approximate the value.\nYou showed the work, and your professor isn't happy.\nHe said that the numbers are too basic, and it needs more decimals.\nSo you attempt to work on it once more.", () => piAccuracy.level >= 10);
 
     updateAvailability();
 }
@@ -68,11 +59,12 @@ var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
     let piBonus = getPiBonus(piAccuracy.level, piExp.level);
+    let c1Value = getC1(c1.level);
+    let c2Value = getC2(c2.level);
     
-    currency.value += dt * bonus * piBonus;
+    currency.value += dt * bonus * piBonus * c1Value * c2Value;
 }
 
-// Calculates the π bonus (caps at 10x)
 var getPiBonus = (level, exponentLevel) => {
     let digits = level + 1;
     let baseBonus = BigNumber.from(1 + Math.log10(1 + digits)).min(10);
@@ -80,8 +72,11 @@ var getPiBonus = (level, exponentLevel) => {
     return baseBonus.pow(exponent);
 }
 
+var getC1 = (level) => BigNumber.from(1 + 0.01 * level);
+var getC2 = (level) => BigNumber.from(1 + 0.02 * level);
+
 var getPrimaryEquation = () => {
-    let result = "\\dot{\\rho} = Bonus(π)";
+    let result = "\\dot{\\rho} = Bonus(π) \\times c_1 \\times c_2";
 
     if (piExp.level == 1) result += "^{1.05}";
     if (piExp.level == 2) result += "^{1.1}";
@@ -93,7 +88,7 @@ var getPrimaryEquation = () => {
 var getSecondaryEquation = () => theory.latexSymbol + "=\\max\\rho";
 var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
 var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
-var getTau = () => currency.value;
+var getTau = () => currency.value.pow(1.2);
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
 init();
