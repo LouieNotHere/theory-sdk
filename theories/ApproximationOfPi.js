99f581812ade6483f37865b8b01634304b18d3ca
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-03-07 18:17:06 +0800

    Update ApproximationOfPi.js

--- theories/ApproximationOfPi.js
+++ theories/ApproximationOfPi.js
@@ -86,7 +86,7 @@ var getPrimaryEquation = () => {
     return result;
 }
 
-var getSecondaryEquation = () => theory.latexSymbol + "=\\max\\rho, \\quad \\rho = " + Utils.getMath(currency.value.toString());
+var getSecondaryEquation = () => theory.latexSymbol + "=\\max\\rho^{1.2}, \\quad \\rho = " + currency.value.toString();
 var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
 var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
 var getTau = () => currency.value.pow(1.2);
