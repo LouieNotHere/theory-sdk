02806fe54e584e724363c8da50ffbea7d7a4a32a
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-03-07 18:13:34 +0800

    Update ApproximationOfPi.js

--- theories/ApproximationOfPi.js
+++ theories/ApproximationOfPi.js
@@ -14,15 +14,15 @@ var currency;
 var piAccuracy, c1, c2;
 var piExp;
 
-var achievement1, achievement2;
-var chapter1, chapter2;
+var achievement1, secretAchievement1, secretAchievement2;
+var chapter1, chapter2, chapter3;
 
 var init = () => {
     currency = theory.createCurrency();
 
     piAccuracy = theory.createUpgrade(0, currency, new ExponentialCost(10, Math.log2(3)));
-    piAccuracy.getDescription = (_) => Utils.getMath("π Digits: " + (piAccuracy.level + 1));
-    piAccuracy.getInfo = (amount) => Utils.getMathTo("Correct Digits: " + (piAccuracy.level + 1), "Correct Digits: " + (piAccuracy.level + amount + 1));
+    piAccuracy.getDescription = (_) => Utils.getMath("dπ = " + (piAccuracy.level + 1));
+    piAccuracy.getInfo = (amount) => Utils.getMathTo("dπ = " + (piAccuracy.level + 1), "dπ = " + (piAccuracy.level + amount + 1));
 
     c1 = theory.createUpgrade(1, currency, new ExponentialCost(15, Math.log2(2)));
     c1.getDescription = (_) => Utils.getMath("c_1=" + getC1(c1.level).toString(2));
@@ -44,7 +44,8 @@ var init = () => {
     piExp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
 
     achievement1 = theory.createAchievement(0, "Basic Approximation", "Correctly approximate at least 3 digits of pi.", () => piAccuracy.level >= 3);
-    achievement2 = theory.createSecretAchievement(1, "Approximator", "Approximate the first 10 digits of pi.", "You're actually into approximating numbers, are you?", () => piAccuracy.level >= 10);
+    secretAchievement1 = theory.createSecretAchievement(1, "Approximator", "Approximate the first 10 digits of pi.", "You're actually into approximating numbers, are you?", () => piAccuracy.level >= 10);
+    secretAchievement2 = theory.createSecretAchievement(2, "Master of Approximation", "Approximate the 100 digits of pi.", "Oh boy...", () => piAccuracy.level >= 100);
 
     chapter1 = theory.createStoryChapter(0, "Prologue", "You are a young mathematician.\nYour professor tells you approximate the value of pi.\nAs you got home, you took a small research about it.\nFortunately, no one has approximated the actual value of pi.\nFor now, you are attempting to approximate the value of pi.\nWill you be able to reach the usual value?", () => piAccuracy.level >= 0);
     chapter2 = theory.createStoryChapter(1, "The Approximation Begins", "You have reached the usual value.\nBut it seemed like it's not enough.\nYou asked your professor about this.\nHe advised you to try and approximate it further.\nSo you went to your desk, and try to do what he said.", () => piAccuracy.level >= 3);
@@ -85,7 +86,7 @@ var getPrimaryEquation = () => {
     return result;
 }
 
-var getSecondaryEquation = () => theory.latexSymbol + "=\\max\\rho";
+var getSecondaryEquation = () => theory.latexSymbol + "=\\max\\rho^{1.2}, \\quad \\rho = " + Utils.getMath(currency.value.toString());
 var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
 var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
 var getTau = () => currency.value.pow(1.2);
